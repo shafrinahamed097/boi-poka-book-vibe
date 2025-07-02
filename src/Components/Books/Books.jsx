3d196c4b5d62ecbda588428d42a6063ed40b28d7
shafrinahamed097 <shafrinahamedsa@gmail.com>
2025-07-02 05:07:29 -0600

    book card and responsive card layout

--- src/Components/Books/Books.jsx
+++ src/Components/Books/Books.jsx
@@ -1,17 +1,21 @@
 import React, { useState, useEffect } from "react";
+import Book from "../Book/Book";
 
 const Books = () => {
   const [books, setBooks] = useState([]);
   useEffect(() => {
-    fetch('./booksData.json')
-    .then(res=>res.json())
-    .then(data=>setBooks(data))
-
+    fetch("./booksData.json")
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
   }, []);
   return (
     <div>
       <h2 className="playfair font-bold text-4xl text-center">Books</h2>
-      <p>{books.length}</p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {
+            books.map(book=> <Book key={book.bookId} book={book} />)
+        }
+      </div>
     </div>
   );
 };
